5f8c29bcf68dc230964ffc785cd057a25d992a0b
knowcodera <knowcodera@gmail.com>
2024-12-02 17:53:20 -0300

    ajuste de connection

--- production/src/ProductionTests/TestMongoDbContext.cs
+++ production/src/ProductionTests/TestMongoDbContext.cs
@@ -5,8 +5,15 @@ namespace ProductionTests
 {
     public static class TestMongoDbContext
     {
-        public static IMongoCollection<ProductionOrder> CreateTestCollection(out MongoClient client, string connectionString = "mongodb://localhost:27017")
+        public static IMongoCollection<ProductionOrder> CreateTestCollection(out MongoClient client)
         {
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("The connection string for MongoDB is not configured.");
+            }
+
             client = new MongoClient(connectionString);
             var database = client.GetDatabase("TestProductionDb");
             var collection = database.GetCollection<ProductionOrder>("ProductionOrders");
